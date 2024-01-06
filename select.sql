7ab1baf9719c971dbb6a83f145cee69035c2dda8
newworksorcerer <>
2024-01-06 19:34:49 +0900

    basic sql select

---
+++ select.sql
@@ -0,0 +1,10 @@
+--01-DB
+--Äõ¸®¹® ÁÖ¼®
+-- "select*from tab;" -> "SELECT * FROM tab;"
+SELECT * FROM tab;
+
+-- "select*from departments;" -> "SELECT * FROM departments;"
+SELECT * FROM departments;
+
+-- "select*from employees;" -> "SELECT * FROM employees;"
+SELECT * FROM employees;
