e59cd4421999df714797b8d472da0cb7fe02db47
networkSorcerer <gigattfyhig@gmail.com>
2024-03-11 19:16:55 +0900

    db

--- 프로젝트_수정.sql
+++ 프로젝트_수정.sql
@@ -28,7 +28,12 @@ comment on column animal.animal_file is '게시판 이미지 경로 및 파일명';
 alter table animal
 add(animal_thumb varchar2(500));
 
-
+--입양 공고 이미지 파일 
+alter table adoption
+add(adoption_file varchar2(500));
+--입양 공공 썸네일 파일 
+alter table  adoption
+add(adoption_thumb varchar2(500));
 
 -- 1. 테이블 구조 변경 (새로운 컬럼 추가)
 ALTER TABLE animal
@@ -56,5 +61,62 @@ SELECT ad.adoption_id, ad.adoption_title, ad.adoption_status, a.animal_id, a.ani
 		ORDER BY ad.adoption_id DESC;
         
         
-        
-        
+create table center (
+    center_id number not null primary key,
+    center_adress varchar2(100) , 
+    center_operating varchar2(100) ,
+    center_tell varchar2(50) ,
+    center_lat number ,
+    center_lot number
+    );
+
+alter table center add center_name varchar2(100);   
+
+select * from center;
+
+ALTER TABLE center
+RENAME COLUMN center_adress TO center_address;
+
+
+CREATE SEQUENCE center_seq
+    START WITH 1
+    INCREMENT BY 1
+    NOCACHE
+    NOCYCLE;
+
+
+insert into center ( center_id,center_name,center_address,center_operating,center_tell,center_lat,center_lot)
+values (center_seq.nextval,'도그마루 보호소',' 서울특별시 송파구 석촌호수로 104','연중무휴 10:00 ~ 20:00','070-4349-3357',37.5077209,127.0884273);
+
+insert into center ( center_id,center_name,center_address,center_operating,center_tell,center_lat,center_lot)
+values (center_seq.nextval,'아이조아',' 서울 용산구 녹사평대로32길 28 2층','연중무휴 12:00~20:30','0507-1316-1160',37.5334362,126.9902071);
+
+insert into center ( center_id,center_name,center_address,center_operating,center_tell,center_lat,center_lot)
+values (center_seq.nextval,'서초 동물 사랑 센터',' 서울특별시 서초구 양재천로19길 22','월요일 휴무 10:00~17:00','02-6956-7980',37.4763321,127.0381241);
+           
+ select * from adoption;
+ 
+select an.animal_id, an.animal_name, an.animal_species, an.animal_gender, an.animal_gender, 
+an.animal_kg, an.animal_age,
+an.animal_age,an.animal_color,an.animal_status,an.animal_memo,an.animal_temp, 
+ad.adoption_id, ad.adoption_title,ad.adoption_level,ad.adoption_status,ad.adoption_date,
+ad.adoption_cnt,ad.adoption_file,ad.adoption_thumb
+from animal an  join adoption ad  on an.animal_id = ad.animal_id;
+
+select * from animal;
+select * from adoption;
+ 
+ALTER TABLE adoption
+DROP COLUMN admin_id;
+
+AlTER TABLE adoption
+DROP COLUMN animal_id;
+
+alter table adoption
+add(adoption_passwd number default 0);
+
+alter table adoption
+add(admin_id number default 0);
+
+INSERT INTO adoption (adoption_id, adoption_title, adoption_level, adoption_status, adoption_content, adoption_passwd)
+VALUES (adoption_seq.nextval, '공고', '상', '공고 중', '입양 강추', '1234');
